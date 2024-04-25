aa0593145ee61d2462829946e3a66ae49573747a
Ziad H <31738365+ziadhassan7@users.noreply.github.com>
2024-04-26 02:18:03 +0300

    [206] [Time Beats: 69.39%] [Memory Beats: 7.14%] - LeetPush

--- 206-Reverse-Linked-List.dart
+++ 206-Reverse-Linked-List.dart
@@ -7,6 +7,49 @@
  *   ListNode([this.val = 0, this.next]); //optional positional as it could be null
  * }
  */
+
+
+//----------Using Recursion
+
+//Code 1
+class Solution {
+  ListNode? reverseList(ListNode? head, [ListNode? prevNode]) {    
+    // Head is null so prev is the last node with a value
+    // Return the last node you have which is the prev node (new head of the reversed list)
+    if (head == null) return prevNode; 
+
+    // Reverse this node
+    var next = head.next;
+    head.next = prevNode;
+
+    // Reverse the next node
+    return reverseList(next, head); 
+  }
+}
+
+
+/*
+//Code 2
+class Solution {
+  ListNode? reverseList(ListNode? head) {
+    return reverse(head, null);
+  }
+
+  ListNode? reverse(ListNode? head, ListNode? reversed) {
+    // Head is null so prev is the last node with a value
+    // Return last node we have, which is the reversed prev node (new head)
+    if (head == null) return reversed;
+    
+    // Reverse this node
+    reversed = ListNode(head.val, reversed); //now save currentHead refering to the prev (reversed)
+    // Reverse the next node
+    return reverse(head?.next, reversed); //nextNode, ref to the current reversed node
+  }
+}
+*/
+
+/*
+//----------Using Loop
 class Solution {
   ListNode? reverseList(ListNode? head) {
     ListNode? prev = null;
@@ -25,3 +68,4 @@ class Solution {
      return prev;
   }
 }
+*/
