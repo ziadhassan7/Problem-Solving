72486828190d475071c54cd6560deebe4b3091f6
Ziad H <31738365+ziadhassan7@users.noreply.github.com>
2024-05-08 01:10:17 +0300

    [17] [Time Beats: 100.00%] [Memory Beats: 90.20%] - LeetPush

--- 17-Letter-Combinations-of-a-Phone-Number.dart
+++ 17-Letter-Combinations-of-a-Phone-Number.dart
@@ -21,10 +21,14 @@ class Solution {
         }
 
 
+        // Get character possibilities of this digit
+        String currentDigit = digits[i];
+        String chars = phone[currentDigit]!;
+
         //Loop through the character possibilities of digit
-        for(String char in phone[digits[i]]){
+        for(int x=0; x<chars.length; x++){
             //Try another candidate
-            dfs(i+1, curStr + char);
+            dfs(i+1, curStr + chars[x]);
         }
 
     }
@@ -33,3 +37,100 @@ class Solution {
     return res;
   }
 }
+
+
+/*
+//My Approach: IT WORKS :)
+
+class Solution {
+  List<String> letterCombinations(String digits) {
+    Map<String, List<String>> phone = {
+        \2\: [\a\, \b\, \c\],
+        \3\: [\d\, \e\, \f\],
+        \4\: [\g\, \h\, \i\],
+        \5\: [\j\, \k\, \l\],
+        \6\: [\m\, \n\, \o\],
+        \7\: [\p\, \q\, \r\, \s\],
+        \8\: [\t\, \u\, \v\],
+        \9\: [\w\, \x\, \y\, \z\],
+    };
+
+    List<String> res = [];
+
+    List<String> comb = [];
+    void dfs(int i){
+        // Base Case: current branch reached the length of our allowed string
+        if(i >= digits.length){
+            res.add(comb.join(\\));
+            return;
+        }
+
+        // Get character possibilities of this digit
+        String currentDigit = digits[i];
+        List<String> chars = phone[currentDigit]!;
+
+        //Loop through the character possibilities of digit
+        for(int x=0; x<chars.length; x++){
+            //Add char
+            comb.add(chars[x]);
+            //Try another candidate
+            dfs(i+1);
+            //Remove Char -BackTrack-
+            comb.removeLast();
+        }
+
+    }
+
+    if(digits.isNotEmpty) dfs(0);
+    return res;
+  }
+}
+*/
+
+/*
+//Using a string: Does Not Work :(
+
+class Solution {
+  List<String> letterCombinations(String digits) {
+    Map<String, List<String>> phone = {
+        \2\: [\a\, \b\, \c\],
+        \3\: [\d\, \e\, \f\],
+        \4\: [\g\, \h\, \i\],
+        \5\: [\j\, \k\, \l\],
+        \6\: [\m\, \n\, \o\],
+        \7\: [\p\, \q\, \r\, \s\],
+        \8\: [\t\, \u\, \v\],
+        \9\: [\w\, \x\, \y\, \z\],
+    };
+
+    List<String> res = [];
+
+    String comb = '';
+    void dfs(int i){
+        // Base Case: current branch reached the length of our allowed string
+        if(i >= digits.length){
+            res.add(comb);
+            return;
+        }
+
+        // Get character possibilities of this digit
+        String currentDigit = digits[i];
+        List<String> chars = phone[currentDigit]!;
+
+        //Loop through the character possibilities of digit
+        for(int x=0; x<chars.length; x++){
+            //Add char
+            comb = comb + chars[x];
+            //Try another candidate
+            dfs(i+1);
+            //Remove Char -BackTrack-
+            comb.substring(0, comb.length - 1);
+        }
+
+    }
+
+    dfs(0);
+    return res;
+  }
+}
+*/
